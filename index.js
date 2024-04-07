b30e44b21e9a61db5b30a6a7e8c17ef0b474546a
brianchege2k <chegebrian84@gmail.com>
2024-04-07 18:03:16 +0300

    sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const data = {
+      name: name,
+      email: email,
+    };
+  
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, options)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was n ot ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        document.body.append(data.id);
+        return data;
+      })
+      .catch(error => {
+        document.body.append(error.message);
+      });
+  }
+  
